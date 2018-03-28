cb24808c7335a26926ec4e5724379659bbf893b2
Macdere <v.dereclenne@gmail.com>
2018-03-28 23:14:10 +0200

    26/03 to 28/03
    Commit of the work done

--- myProject/Assets/Scripts/cameraFollow.cs
+++ myProject/Assets/Scripts/cameraFollow.cs
@@ -4,22 +4,35 @@ using UnityEngine;
 
 public class cameraFollow : MonoBehaviour {
 
+	[Header("Target")]
 	public Transform target;
-
-	public float smoothSpeed = 0.125f;
+	[Header("Distances")]
+	public float distance = 3f;
+	public float minDistance = 1.5f;
+	public float maxDistance = 6f;
 	public Vector3 offset;
+	[Header("Speeds")]
+	public float smoothSpeed = 5f;
+	public float scrollSensitivity = 1;
 
-	void FixUpdate (){
+	void Start(){
 
+	}
+
+	void LateUpdate(){
 		if (!target) {
-			print ("No target found !");
+			print ("No target found");
 			return;
 		}
 
-		Vector3 desiredPosition = target.position + offset;
-		Vector3 smoothedPosition = Vector3.Lerp (transform.position, desiredPosition, smoothSpeed);
-		transform.position = smoothedPosition;
+		float num = Input.GetAxis ("Mouse ScrollWheel");
+		distance -= num * scrollSensitivity;
+		distance = Mathf.Clamp (distance, minDistance, maxDistance);
 
-		transform.LookAt(target);
+		Vector3 pos = target.position + offset;
+		pos -= transform.forward * distance;
+
+		transform.position = Vector3.Lerp (transform.position, pos, smoothSpeed * Time.deltaTime);
+
 	}
 }
---
+++ myProject/Assets/clickAndMove.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class clickAndMove : MonoBehaviour {
+	[Header("Stats")]
+	public float attackDistance;
+	public float attackRate;
+	private float nextAttack;
+	const float k_Half = 0.5f;
+	float m_ForwardAmount;
+
+	private NavMeshAgent agent;
+	private Animator m_Animator;
+
+	private Transform targetedEnemy;
+	private bool enemyClicked;
+	private bool m_IsGrounded;
+	// Use this for initialization
+	void Awake () {
+		m_Animator = GetComponent<Animator> ();
+		agent = GetComponent<NavMeshAgent> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		RaycastHit hit;
+
+		if (Input.GetButtonDown("Fire2")) {
+			if(Physics.Raycast(ray, out hit, 1000)){
+				if(hit.collider.tag == "Enemy"){
+					targetedEnemy = hit.transform;
+					enemyClicked = true;
+				}
+				else{
+					m_IsGrounded = true;
+					enemyClicked = false;
+					agent.isStopped = false;
+					agent.destination = hit.point;
+				}
+			}
+		}
+		if (enemyClicked){
+			moveAndAttack ();	
+		}
+		if(agent.remainingDistance <= agent.stoppingDistance){
+					m_IsGrounded = false;
+		}
+		else{
+					m_IsGrounded = true;
+		}
+			
+		m_Animator.SetBool("isWalking", m_IsGrounded);
+	}
+	void moveAndAttack(){
+		if (targetedEnemy == null) {
+			return;
+		}
+
+		agent.destination = targetedEnemy.position;
+
+		if (agent.remainingDistance > attackDistance) {
+			agent.isStopped = false;
+			m_IsGrounded = true;
+		} else {
+			transform.LookAt (targetedEnemy);
+			//Vector3 dirAttack = targetedEnemy.transform.position - transform.position;
+				if(Time.time > nextAttack){
+					nextAttack = Time.time + attackRate;
+				}
+			agent.isStopped = true;
+			m_IsGrounded = false;
+		}
+	}
+}
