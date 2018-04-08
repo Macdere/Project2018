343962418f05a771dedfbeeebfd5b5800c41243e
Macdere <v.dereclenne@gmail.com>
2018-04-08 17:02:36 +0200

    08/04/18
    Update I just modified lot of things like menu"s lights ...

---
+++ myProject/Assets/Scripts/mainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainMenu : MonoBehaviour {
+
+	public void PlayGame(){
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+
+	public void LoadGame(){
+		Debug.Log ("Load your Game");
+	}
+
+	public void QuitGame(){
+
+		Debug.Log ("You Quit the Game");
+		Application.Quit ();
+	}
+}
