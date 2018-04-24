b344d25fba396f854890333a102bc8a5c6fa5e93
Macdere <v.dereclenne@gmail.com>
2018-04-24 16:49:49 +0200

    23-24/04
    Just forgot to update on Monday, creation of interactable class, manage the comportement of the player with those objects. getting informations for the AI of an Enemy and how ennemies work with Unity.

---
+++ myProject/Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour {
+
+	public LayerMask movementMask;
+	Camera cam;
+	public Interactable focus;
+
+	// Use this for initialization
+	void Start () {
+		cam = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetButtonDown("Horizontal") || Input.GetButtonDown("Vertical")){
+			Ray ray = cam.ScreenPointToRay (Input.mousePosition);
+			RaycastHit hit;
+
+
+			if(Physics.Raycast(ray,out hit)){
+				Interactable interactable = hit.collider.GetComponent<Interactable> ();
+				if (interactable == null) {
+					removeFocus ();
+				}
+			}
+		}
+
+		if(Input.GetMouseButtonDown(1)){
+			Ray ray = cam.ScreenPointToRay (Input.mousePosition);
+			RaycastHit hit;
+
+			if(Physics.Raycast(ray,out hit)){
+				Interactable interactable = hit.collider.GetComponent<Interactable> ();
+				if (interactable != null) {
+					setFocus (interactable);
+				}
+			}
+		}
+	}
+
+	void setFocus(Interactable newFocus){
+		focus = newFocus;
+	}
+
+	void removeFocus(){
+		focus = null;
+	}
+}
---
+++ myProject/Assets/Scripts/gameControl.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.IO;
+
+public class gameControl : MonoBehaviour {
+
+	public static gameControl control;
+
+	public float cSharpKnowledge;
+	public float unityKnowledge;
+	public float threeDKnowledge;
+
+	// Use this for initialization
+	void Awake () {
+		if (control == null) {
+			DontDestroyOnLoad (gameObject);
+			control = this;
+		} else if (control != this) {
+			Destroy (gameObject);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+	// Test of data
+	void OnGUI(){
+		GUI.Label (new Rect (10, 10, 100, 30), "C Shap : " + cSharpKnowledge);
+	}
+}
