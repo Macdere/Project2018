0926bee3433d41f67dbeee8d69375d46f83ad810
Macdere <v.dereclenne@gmail.com>
2018-05-02 18:23:12 +0200

    02/05
    Creation of Enemies and AI attached to it. EnemyController add to manage the movement of our enemy

---
+++ myProject/Assets/Scripts/EnemyStats.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStats : characterStats {
+
+	public override void Die(){
+		base.Die ();
+		// Add some animations or some for the death of the enemy
+
+		Destroy(gameObject);
+	}
+
+}
