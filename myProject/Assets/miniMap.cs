7ce5b5cafc9bcb2a52025fe6f1fbea35d95c9332
Macdere <v.dereclenne@gmail.com>
2018-05-09 19:05:38 +0200

    09/05
    Mini map done, follow th player and work with a script using Vector3.

---
+++ myProject/Assets/miniMap.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class miniMap : MonoBehaviour {
+
+	public Transform player;
+
+	// Update is called once per frame
+	void LateUpdate () {
+		Vector3 newPosition = player.position;
+		newPosition.y = transform.position.y;
+		transform.position = newPosition;
+
+		// if I need the camera to rotate with the player.
+		//transform.rotation = Quaternion.Euler (90f, player.eulerAngles.y, 0f);
+	}
+}
