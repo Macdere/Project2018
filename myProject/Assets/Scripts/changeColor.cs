67f731826b4d0dcd84d0cb176cdf3fb543b21a29
Macdere <v.dereclenne@gmail.com>
2018-05-16 22:54:26 +0200

    16/05
    Settings Scene added to the game

--- myProject/Assets/Scripts/changeColor.cs
+++ myProject/Assets/Scripts/changeColor.cs
@@ -1,22 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class changeColor : MonoBehaviour {
 
 	public Color startColor;
+	public Color newColor;
+	public float duration = 1.0F;
+	Renderer rend;
 
 	bool mouseOver = false;
+
+	public void Start(){
+		rend = GetComponent<Renderer> ();
+	}
 	// Use this for initialization
 	void OnMouseEnter () {
 		mouseOver = true;
-		GetComponentInChildren<Renderer> ().material.color = Color.green;
+		float lerp = Mathf.PingPong(Time.time, duration) / duration;
+		rend.material.color = Color.Lerp(startColor, newColor, lerp);
 	}
 	
 	// Update is called once per frame
 	void OnMouseExit () {
 		mouseOver = false;
-		GetComponentInChildren<Renderer> ().material.color = startColor;
+		float lerp = Mathf.PingPong(Time.time, duration) / duration;
+		rend.material.color = Color.Lerp(newColor, startColor, lerp);
 	}
 
 	// Don't work I don'tknow why
---
+++ myProject/Assets/continueButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class continueButton : MonoBehaviour {
+
+	public string nameScene;
+
+	public void Next(){
+		SceneManager.LoadScene (nameScene);
+	}
+}
